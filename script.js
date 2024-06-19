82d0766380befc9cee7c2d000a54157284ca8aaa
Daniela123sch <116928392+Daniela123sch@users.noreply.github.com>
2024-06-19 19:42:15 -0300

    ss

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+  perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+      mostraAlternativas();
+      }
+      function mostraAlternativas() {}
+
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+      }
+      }
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+            botaoAlternativa.textContent = alternativa;
+                caixaAlternativas.appendChild(botaoAlternativas);
+                  }
+                  }
+
+
+
 
                                                                  
                                               
